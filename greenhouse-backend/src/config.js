90e9a6f225e378654968e4842cb47de6c90aecf7
nameless05105 <34394939+nameless05105@users.noreply.github.com>
2021-05-20 14:40:31 +0300

    Delete config.js

--- greenhouse-backend/src/config.js
+++
@@ -1,9 +0,0 @@
-export const {
-    PORT = 5000,
-    NODE_ENV = 'development',
-    // MONGO_URI = 'mongodb://localhost/data',
-    MONGO_URI = 'mongodb://SuperNora9:tYf74-Gnet5_yhx@95.181.230.220:38128/data?authSource=admin',
-    SESS_NAME = 'sid',
-    SESS_SECRET = 'secret!session',
-    SESS_LIFETIME = 1000 * 60 * 60 * 2
-} = process.env;
